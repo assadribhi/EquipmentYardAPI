70dae3f861698d8c32d32be28e24c787bb9f9146
assadribhi <assadribhi@gmail.com>
2020-07-20 22:40:37 +0300

    Create Item Added

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const slugify = require("slugify");
 
 let equipment = require("./equipment");
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/equipment", (req, res) => {
   res.json(equipment);
+});
+
+app.post("/equipment", (req, res) => {
+  const id = equipment[equipment.length - 1].id + 1;
+  const slug = slugify(req.body.name, { lower: true });
+  const newEquipment = { id, slug, ...req.body };
+  equipment.push(newEquipment);
+  res.status(201).json(newEquipment);
 });
 
 app.delete("/equipment/:equipmentId", async (req, res) => {
