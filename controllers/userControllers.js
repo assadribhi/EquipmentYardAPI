0b8902357ee5a85fdcb75a6bd549de55f2c4286e
assadribhi <assadribhi@gmail.com>
2020-08-10 20:01:14 +0300

    added the role to the payload

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -8,6 +8,7 @@ exports.signIn = (req, res) => {
   const payload = {
     id: user.id,
     username: user.username,
+    role: user.role,
     expires: Date.now() + JWT_EXPIRATION_MS,
   };
   const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
