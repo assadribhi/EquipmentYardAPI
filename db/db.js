0b822d0d31b1933c86e190b6744366b527aad51c
assadribhi <assadribhi@gmail.com>
2020-08-13 20:50:33 +0300

    FIXED DATABASE configration

--- db/db.js
+++ db/db.js
@@ -1,11 +1,16 @@
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize({
-  username: "postgres",
-  password: "@$$@D@$$@d",
-  database: "equipmentYard_db",
-  dialect: "postgres",
-  host: "localhost",
-  logging: false,
-});
+const db = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      logging: false,
+    })
+  : new Sequelize({
+      username: "postgres",
+      password: "@$$@D@$$@d",
+      database: "equipmentYard_db",
+      dialect: "postgres",
+      host: "localhost",
+      logging: false,
+    });
 module.exports = db;
